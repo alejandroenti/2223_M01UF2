8454a53c81d335139a5db07bd0e572f2c933192b
Alejandro Lopez <alejandro.lopez@enti.cat>
2022-11-07 10:13:43 +0100

    Acabando de implementar el handshake

--- cliente.sh
+++ cliente.sh
@@ -4,3 +4,17 @@ echo "Cliente TURIP"
 
 echo "(1) SEND MSG: HOLI_TURIP 127.0.0.1"
 echo "HOLI_TURIP 127.0.0.1" | nc localhost 2223
+
+# Nos ponemos en escucha por el puerto 2223 para recibir la respuesta
+
+ERROR=`nc -l 2223`
+
+# Validamos si el mensaje recibido es OK_TURIP
+# En caso de recibir KO_TURIP, mostramos mensaje de error y salimos
+#	con el código de estado 1
+
+if [ "$ERROR" != "OK_TURIP" ]
+then
+	echo "ERROR 1: Handshake incorrecto"
+	exit 1
+fi
--- servidor.sh
+++ servidor.sh
@@ -11,10 +11,18 @@ echo "(0) LISTEN"
 MSG=`nc -l 2223`
 
 # Revisamos si el mensaje recibido es correcto
+# Para ello separados el código de error recibido de la IP
 # En caso de no ser correcto, mostraremos un mensaje de ERROR
 # 	y salimos con el código de estado 1
-if [ "$MSG" != "HOLI_TURIP" ]
+
+ERRORCLIENTE=`echo $MSG | cut -d " " -f 1`
+IPCLIENTE=`echo $MSG | cut -d " " -f 1`
+
+if [ "$ERRORCLIENTE" != "HOLI_TURIP" ]
 then
 	echo "ERROR 1: Handshake incorrecto"
+	echo "KO_TURIP" | nc localhost 2223
 	exit 1
+else
+	echo "OK_TURIP" | nc localhost 2223
 fi
